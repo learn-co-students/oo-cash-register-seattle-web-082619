60473c076baae2235c26f357b029a43d5202655f
Ian Harold <ianhunterharold@gmail.com>
2019-09-02 18:34:17 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,15 +7,19 @@ class CashRegister
         self.discount= discount 
     end 
 
-    def add_item(title, price, quantity= 1) 
-        @total += price * quantity 
+    def add_item(title, price, quantity = 1) 
+        self.total += price * quantity 
     end 
 
     def apply_discount
-        total = total*((100.0 - discount.to_f)/100).to_i
-        # we want everything in parantheses to be .20
-        #binding.pry
-    end 
+      if discount != 0
+         self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+        "After the discount, the total comes to $#{self.total}."
+      else
+        "There is no discount to apply."
+      end
+  end
+  
 
 
 
